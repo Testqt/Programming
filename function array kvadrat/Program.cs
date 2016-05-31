c92cced2c458d7d6b061b9e5fdbdc074284f9b96
Testqt <lenusya.kolosova@gmail.com>
2016-05-31 15:02:40 +0300

    first version

---
+++ function array kvadrat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace function_array_kvadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]ar = new int[10];
+            Random ran = new Random();
+            
+            for (int i = 0; i <ar.Length; i++)
+            {
+                ar[i] = ran.Next(-100,100);
+                Console.WriteLine(ar[i]);
+            }
+            Console.WriteLine(" ");
+
+            int rez = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                rez = kvadrat(ar[i]);
+                Console.WriteLine(ar[i]);
+            }
+            Console.ReadLine();
+
+        }
+
+        public static int kvadrat(int kv)
+        {           
+            int rez = kv*kv;
+            return rez;
+        }
+    }
+}
